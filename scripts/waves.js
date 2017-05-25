64ccdabca0314f1e7dc483160985ab252cf6bb81
r2ysxu <r2ysxu@gmail.com>
2017-05-25 12:50:31 -0400

    Add scrollbar width buffer to wave canvas

--- scripts/waves.js
+++ scripts/waves.js
@@ -7,11 +7,11 @@ $( document ).ready(function() {
 	    var cameraEl = $('#waves-camera')[0];
 	    var simulator = new Simulator(canvasEl, window.innerWidth, window.innerHeight);
 
-	    var width = document.body.clientWidth,
+	    var width = document.body.clientWidth + 20,
         	height = window.innerHeight;
 
 	    var onresize = function () {
-	        var windowWidth = document.body.clientWidth,
+	        var windowWidth = document.body.clientWidth + 20,
 	        windowHeight = FIXED_CANVAS_HEIGHT;
 
 	        if (windowWidth / windowHeight > MIN_ASPECT) {
